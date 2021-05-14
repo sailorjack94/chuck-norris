03f1a984e3d152a614a0f2dd41bb0e9e0b88a854
sailorj94 <jackniall9@gmail.com>
2021-05-14 14:49:41 +0100

    Styling and prep for deployment

--- src/components/GetJoke.js
+++ src/components/GetJoke.js
@@ -12,7 +12,7 @@ const GetJoke = ({anotherJoke}) => {
 
     return(
         <div className = 'button-wrapper'>
-        <button onClick={moreJokes}>more facts.</button>
+        <button className='btn' onClick={moreJokes}>more facts.</button>
         </div>
     )
 }
